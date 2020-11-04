2f933268a7a9f1d196fddc3f3898cf61181521de
Roee Angel <roee.angel93@gmail.com>
2020-11-04 22:17:02 +0200

    done!

--- src/Components/DisplayMSG.js
+++ src/Components/DisplayMSG.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayMSG.css";
 export default function DisplayMSG() {
+  let [ArrayOfMsg, SetArrayOfMsg] = useState([]);
+  let [search, SetSearch] = useState("");
   let displaydata = null;
   if (localStorage.getItem("ArrayOfMsg")) {
     displaydata = JSON.parse(localStorage.getItem("ArrayOfMsg")).map((e) => {
-      return (
-        <div className="row">
-          <img src={e.img}></img>
-          <div className="col">
-            <div className="email">{e.email}</div>
-            <div>{e.message}</div>
+      if (e.email.includes(search)) {
+        return (
+          <div className="row" key={e.email}>
+            <img src={e.img}></img>
+            <div className="col">
+              <div className="email">{e.email}</div>
+              <div>{e.message}</div>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     });
   }
+  //   useEffect(() => {
+  //     ArrayOfMsg = [...JSON.parse(localStorage.getItem("ArrayOfMsg"))];
+  //   });
   return (
     <div className="displayMsg-wraper">
-      <input></input>
+      <input
+        placeholder="&#128270; Search"
+        onChange={(e) => SetSearch(e.target.value)}
+      ></input>
       <div>{displaydata ? displaydata : "LOADING"}</div>
     </div>
   );
