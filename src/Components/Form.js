aeb56ef5434d4a01b7e72b97735e8b4b56bcd885
Roee Angel <roee.angel93@gmail.com>
2020-11-04 21:00:07 +0200

    add logic to form component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import faker from "Faker";
 import "./Form.css";
 export default function Form() {
+  const [email, SetEmail] = useState("");
+  const [message, SetMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && message) {
+      if (localStorage) console.log(email, message, faker.Image.avatar());
+    }
+  };
   return (
     <div className="form-wraper">
-      <form className="form">
-        <input placeholder="Email"></input>
-        <input placeholder="Message" className="msg-input"></input>
+      <form className="form" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          placeholder="Email"
+          onChange={(e) => SetEmail(e.target.value)}
+        ></input>
+        <input
+          placeholder="Message"
+          className="msg-input"
+          onChange={(e) => SetMessage(e.target.value)}
+        ></input>
         <button>Submit</button>
       </form>
     </div>
